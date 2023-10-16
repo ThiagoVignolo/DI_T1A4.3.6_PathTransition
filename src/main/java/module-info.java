b82b30ad9acb711d482273d60e88a2e51b46b159
Thiago <thiagovignoloferreira@gmail.com>
2023-10-16 20:24:55 +0200

    First.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.di_t1a436_pathtransition {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.di_t1a436_pathtransition to javafx.fxml;
+    exports es.ieslosmontecillos.di_t1a436_pathtransition;
+}
